e00b153113bee5daa8407d2e0c7f39b0985a6857
Jahel <jahelcarmonavila@gmail.com>
2019-08-07 21:19:27 +0200

    Quito ficheros para testear servicios

--- NAO/pahoexample.py
+++
@@ -1,6 +0,0 @@
-import paho.mqtt.client as mqtt #import the client1
-#broker_address="192.168.1.184" 
-broker_address="iot.eclipse.org" #use external broker
-client = mqtt.Client("P1") #create new instance
-client.connect(broker_address) #connect to broker
-client.publish("house/main-light","Holaa soc jaheeel")#publish
